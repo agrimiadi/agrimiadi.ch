b8c111c9a2132f53a93f6786212112ae9b2b4e49
agrimiadi <info@agrimiadi.ch>
2023-01-22 12:48:19 +0100

    intial commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
+  css: {
+    postcss,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    outDir: path.join(__dirname, "docs"),
+  },
+});

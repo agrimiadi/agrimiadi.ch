908f598441fa28beb3303d73e0deaf473fd2bf41
agrimiadi <info@agrimiadi.ch>
2023-02-19 21:24:57 +0100

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,6 @@ import Header from "../partials/Header";
 import HeroHome from "../partials/HeroHome";
 import FeaturesHome from "../partials/Features";
 import FeaturesBlocks from "../partials/FeaturesBlocks";
-import Testimonials from "../partials/Testimonials";
-import Newsletter from "../partials/Newsletter";
-import Footer from "../partials/Footer";
-import Banner from "../partials/Banner";
 
 function Home() {
   return (

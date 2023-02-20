4585b30fe3bc7edd27f7bde736842e2e5ae26376
agrimiadi <info@agrimiadi.ch>
2023-02-20 05:11:40 +0100

    wip

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
-import Modal from "../utils/Modal";
+import React from "react";
 
 import FamilyImage from "../images/family-miadi.jpeg";
 
 function HeroHome() {
-  const [videoModalOpen, setVideoModalOpen] = useState(false);
-
   return (
     <section className="relative">
-      {/* Illustration behind hero content */}
-      {/* <div
-        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
-        aria-hidden="true"
-      >
-        <svg
-          width="1360"
-          height="578"
-          viewBox="0 0 1360 578"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <linearGradient
-              x1="50%"
-              y1="0%"
-              x2="50%"
-              y2="100%"
-              id="illustration-01"
-            >
-              <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#illustration-01)" fillRule="evenodd">
-            <circle cx="1232" cy="128" r="128" />
-            <circle cx="155" cy="443" r="64" />
-          </g>
-        </svg>
-      </div> */}
-
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
@@ -57,11 +23,11 @@ function HeroHome() {
                 data-aos-delay="150"
               >
                 Eine etwas verrückte Geschichte einer Familie die ins abgelegene
-                Calancatal zieht um ihren Traum vom eigenen Bergbauernbetrieb zu
-                verwirklichen. In den Hauptrollen sehen wir - Sarah als
-                Betriebsleiterin, den getreuen Knecht und Ehemann Curdin, den
-                kleinen Jon und nicht zu vergessen den etwas durchgeknallten
-                Bordercollie Sky.
+                Calancatal zieht um ihren Traum vom eigenen Bauernbetrieb im
+                Berggebiet zu verwirklichen. In den Hauptrollen sehen wir -
+                Sarah als Betriebsleiterin, den getreuen Knecht und Ehemann
+                Curdin, den kleinen Jon und nicht zu vergessen den etwas
+                durchgeknallten Bordercollie Sky.
               </p>
             </div>
           </div>
--- vite.config.js
+++ vite.config.js
@@ -27,5 +27,5 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
   },
-  base: "/agrimiadi.ch/",
+  // base: "/agrimiadi.ch/",
 });

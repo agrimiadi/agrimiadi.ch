6a7fcd8e61eac1a0f2b9ca1f56b54b07da1c5928
agrimiadi <info@agrimiadi.ch>
2023-02-25 10:51:59 +0100

    wip

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -10,6 +10,45 @@ function StyledLink(props) {
   return <a className="underline" {...props} />;
 }
 
+function PickUpOrder() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+        />
+      </svg>
+      <div>
+        Abholung /{" "}
+        <StyledLink
+          href="https://www.post.ch/de/pakete-versenden/express-und-kurier/swiss-express-mond"
+          target="_blank"
+        >
+          Post
+        </StyledLink>{" "}
+        (22 CHF)
+      </div>
+    </>
+  );
+}
+
+function WhatsAppGroupLink() {
+  return (
+    <StyledLink href="https://chat.whatsapp.com/FG8ccsdBPtYCortlZ5mKAf">
+      WhatsApp Gruppe
+    </StyledLink>
+  );
+}
+
 function HeroHome() {
   return (
     <>
@@ -196,8 +235,13 @@ function HeroHome() {
               <p className="text-lg text-gray-800">
                 Wir produzieren nicht in grossen Mengen, dafür mit viel
                 Qualität. Falls Dir gefällt, wie und was wir tun, findest Du
-                vielleicht etwas Feines. Fleisch wird jeweils per Express über
-                Nacht versendet. Die Liefergebühren sind im Preis inbegriffen.
+                vielleicht etwas Feines. Fleisch wird jeweils per{" "}
+                <StyledLink href="" target="_blank">
+                  Swiss-Express «Mond»{" "}
+                </StyledLink>
+                über Nacht versendet oder kann tiefgefroren abgeholt werden.
+                Falls Du Fragen hast komm doch in usere <WhatsAppGroupLink />{" "}
+                oder ruf uns direkt an auf 079 756 71 81.
               </p>
             </div>
 
@@ -229,7 +273,7 @@ function HeroHome() {
                       d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.031.352 5.988 5.988 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 01-2.031.352 5.989 5.989 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971z"
                     />
                   </svg>
-                  <div>5kg / 10kg</div>
+                  <div> 4kg / 8kg</div>
                 </div>
                 <div className="pt-4 flex space-x-2">
                   <svg
@@ -249,20 +293,9 @@ function HeroHome() {
                   <div>35 CHF pro Kilo</div>
                 </div>
                 <div className="pt-4 flex space-x-2">
-                  {/* <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg> */}
+                  <PickUpOrder />
+                </div>
+                <div className="pt-4 flex space-x-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -325,7 +358,10 @@ function HeroHome() {
                       d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
                     />
                   </svg>
-                  <div>30 CHF pro Kilo</div>
+                  <div>37 CHF pro Kilo</div>
+                </div>
+                <div className="pt-4 flex space-x-2">
+                  <PickUpOrder />
                 </div>
                 <div className="pt-4 flex space-x-2">
                   {/* <svg
@@ -402,8 +438,12 @@ function HeroHome() {
                       d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
                     />
                   </svg>
-                  <div>20 CHF pro Kilo</div>
+                  <div>Auf Anfrage</div>
                 </div>
+                <div className="pt-4 flex space-x-2">
+                  <PickUpOrder />
+                </div>
+
                 <div className="pt-4 flex space-x-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -464,7 +504,24 @@ function HeroHome() {
                       d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
                     />
                   </svg>
-                  <div>18 CHF pro Kilo</div>
+                  <div>25 CHF pro Kilo</div>
+                </div>
+                <div className="pt-4 flex space-x-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+                    />
+                  </svg>
+                  <div>Abholung / Versand auf Anfrage</div>
                 </div>
                 <div className="pt-4 flex space-x-2">
                   <svg
@@ -502,10 +559,7 @@ function HeroHome() {
             <h2 className="h2 mb-4 text-grey-800">SEI TEIL DES ABENTEUERS</h2>
             <p className="text-lg text-left text-gray-800">
               Möchtest Du Teil des Abenteuers werden, dann komm in unsere{" "}
-              <StyledLink href="https://chat.whatsapp.com/FG8ccsdBPtYCortlZ5mKAf">
-                WhatsApp Gruppe
-              </StyledLink>{" "}
-              und folge uns auf{" "}
+              <WhatsAppGroupLink /> und folge uns auf{" "}
               <StyledLink href="https://www.instagram.com/agrimiadi">
                 Instagram
               </StyledLink>{" "}
